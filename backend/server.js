fa1f3ed96380a80c89e05533daac8cc0e6d5b7c8
SachinKharat-Developer <kharatsukmar@gmail.com>
2025-09-26 03:17:34 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cors());
 
 // MongoDB connection
-mongoose.connect("mongodb://127.0.0.1:27017/realestate")
+mongoose.connect("process.env.MONGO_URI")
   .then(() => console.log("✅ MongoDB connected"))
   .catch(err => console.error("❌ MongoDB error:", err));
 
